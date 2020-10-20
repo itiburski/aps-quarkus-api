beaa2d93b0af23846d9d983b19dba009c5c19531
Isabel Tiburski <itiburski@gmail.com>
2020-10-20 08:34:53 -0300

    default method findByUid in APSRepository

--- src/main/java/br/com/jitec/aps/data/repository/APSRepository.java
+++ src/main/java/br/com/jitec/aps/data/repository/APSRepository.java
@@ -1,8 +1,16 @@
 package br.com.jitec.aps.data.repository;
 
-import br.com.jitec.aps.data.model.APSEntity;
+import java.util.Optional;
+import java.util.UUID;
 
-public interface APSRepository<T extends APSEntity> {
+import br.com.jitec.aps.data.model.APSEntity;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+public interface APSRepository<T extends APSEntity> extends PanacheRepository<T> {
+
+	default Optional<T> findByUid(UUID uid) {
+		return find("uid", uid).firstResultOptional();
+	}
 
 	int customUpdate(T entity);
 
--- src/main/java/br/com/jitec/aps/data/repository/CategoriaClienteRepository.java
+++ src/main/java/br/com/jitec/aps/data/repository/CategoriaClienteRepository.java
@@ -1,20 +1,11 @@
 package br.com.jitec.aps.data.repository;
 
-import java.util.Optional;
-import java.util.UUID;
-
 import javax.enterprise.context.ApplicationScoped;
 
 import br.com.jitec.aps.data.model.CategoriaCliente;
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
 @ApplicationScoped
-public class CategoriaClienteRepository
-		implements APSRepository<CategoriaCliente>, PanacheRepository<CategoriaCliente> {
-
-	public Optional<CategoriaCliente> findByUid(UUID uid) {
-		return find("uid", uid).firstResultOptional();
-	}
+public class CategoriaClienteRepository implements APSRepository<CategoriaCliente> {
 
 	@Override
 	public int customUpdate(CategoriaCliente categ) {
