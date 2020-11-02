b4b1cdba077c761aeeeacd97ebe7d84d72dbb26c
Isabel Tiburski <itiburski@gmail.com>
2020-11-02 20:30:33 -0300

    Improving GenericExceptionMapper

--- src/main/java/br/com/jitec/aps/rest/exception/GenericExceptionMapper.java
+++ src/main/java/br/com/jitec/aps/rest/exception/GenericExceptionMapper.java
@@ -1,5 +1,6 @@
 package br.com.jitec.aps.rest.exception;
 
+import javax.ws.rs.ProcessingException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
@@ -15,8 +16,8 @@ import br.com.jitec.aps.rest.payload.response.ErrorResponse;
 @Provider
 public class GenericExceptionMapper implements ExceptionMapper<Exception> {
 
-	private static final String GENERIC_MESSAGE = "Ocorreu um erro ao processar a sua requisição";
-	private static final StatusType STATUS = Status.INTERNAL_SERVER_ERROR;
+	private static final String INTERNAL_SERVER_ERROR_DETAIL = "Ocorreu um erro ao processar a sua requisição";
+	private static final String BAD_REQUEST_DETAIL = "Erro na requisição: %s";
 
 	private static final Logger LOG = Logger.getLogger(GenericExceptionMapper.class);
 
@@ -25,10 +26,26 @@ public class GenericExceptionMapper implements ExceptionMapper<Exception> {
 
 	@Override
 	public Response toResponse(Exception exception) {
+		if (exception instanceof ProcessingException) {
+			String message = getExceptionMessage(exception);
+			return handleException(exception, Status.BAD_REQUEST, String.format(BAD_REQUEST_DETAIL, message));
+		}
+
 		LOG.error("error", exception);
-		ErrorResponse error = ErrorResponse.builder().withStatus(STATUS.getStatusCode())
-				.withTitle(STATUS.getReasonPhrase()).withDetail(GENERIC_MESSAGE)
-				.withInstance(uriInfo.getRequestUri()).build();
-		return Response.status(STATUS).entity(error).build();
+		return handleException(exception, Status.INTERNAL_SERVER_ERROR, INTERNAL_SERVER_ERROR_DETAIL);
+	}
+
+	private Response handleException(Exception exception, StatusType status, String detail) {
+		ErrorResponse error = ErrorResponse.builder().withStatus(status.getStatusCode())
+				.withTitle(status.getReasonPhrase()).withDetail(detail).withInstance(uriInfo.getRequestUri())
+				.build();
+		return Response.status(status).entity(error).build();
+	}
+
+	private String getExceptionMessage(Throwable exception) {
+		if (exception.getCause() != null) {
+			return getExceptionMessage(exception.getCause());
+		}
+		return exception.getMessage();
 	}
 }
