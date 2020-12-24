c3a8790c604ac1f2cbee794df39e12e993f9e04f
Isabel Tiburski <itiburski@gmail.com>
2020-12-24 08:50:12 -0300

    removing unused mapper methods

--- src/main/java/br/com/jitec/aps/rest/payload/mapper/ClienteEmailMapper.java
+++ src/main/java/br/com/jitec/aps/rest/payload/mapper/ClienteEmailMapper.java
@@ -5,16 +5,10 @@ import java.util.List;
 import org.mapstruct.Mapper;
 
 import br.com.jitec.aps.business.data.ClienteEmailDTO;
-import br.com.jitec.aps.data.model.ClienteEmail;
 import br.com.jitec.aps.rest.payload.request.ClienteEmailRequest;
-import br.com.jitec.aps.rest.payload.response.ClienteEmailResponse;
 
 @Mapper(config = QuarkusMapperConfig.class)
 public interface ClienteEmailMapper {
-
-	ClienteEmailResponse toResponse(ClienteEmail clienteEmail);
-
-	List<ClienteEmailResponse> toListResponse(List<ClienteEmail> clientesEmail);
 
 	ClienteEmailDTO toDto(ClienteEmailRequest clienteEmail);
 
