2d2380970743fdfe0f25eec549b336c080ea502a
Isabel Tiburski <itiburski@gmail.com>
2020-12-24 15:33:53 -0300

    Switch off MapStruct Builder detection

--- src/main/java/br/com/jitec/aps/rest/payload/mapper/ClienteEmailMapper.java
+++ src/main/java/br/com/jitec/aps/rest/payload/mapper/ClienteEmailMapper.java
@@ -2,12 +2,13 @@ package br.com.jitec.aps.rest.payload.mapper;
 
 import java.util.List;
 
+import org.mapstruct.Builder;
 import org.mapstruct.Mapper;
 
 import br.com.jitec.aps.business.data.ClienteEmailDTO;
 import br.com.jitec.aps.rest.payload.request.ClienteEmailRequest;
 
-@Mapper(config = QuarkusMapperConfig.class)
+@Mapper(config = QuarkusMapperConfig.class, builder = @Builder(disableBuilder = true))
 public interface ClienteEmailMapper {
 
 	ClienteEmailDTO toDto(ClienteEmailRequest clienteEmail);
--- src/main/java/br/com/jitec/aps/rest/payload/mapper/ClienteTelefoneMapper.java
+++ src/main/java/br/com/jitec/aps/rest/payload/mapper/ClienteTelefoneMapper.java
@@ -2,12 +2,13 @@ package br.com.jitec.aps.rest.payload.mapper;
 
 import java.util.List;
 
+import org.mapstruct.Builder;
 import org.mapstruct.Mapper;
 
 import br.com.jitec.aps.business.data.ClienteTelefoneDTO;
 import br.com.jitec.aps.rest.payload.request.ClienteTelefoneRequest;
 
-@Mapper(config = QuarkusMapperConfig.class)
+@Mapper(config = QuarkusMapperConfig.class, builder = @Builder(disableBuilder = true))
 public interface ClienteTelefoneMapper {
 
 	ClienteTelefoneDTO toDto(ClienteTelefoneRequest clienteTelefone);
