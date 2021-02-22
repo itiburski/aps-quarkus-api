49b176f199e03da207bf87e56d173901daaf578b
Isabel Tiburski <itiburski@gmail.com>
2021-02-22 08:11:39 -0300

    remove unused fields from request payload

--- cadastro/src/main/java/br/com/jitec/aps/cadastro/rest/payload/request/ClienteTelefoneRequest.java
+++ cadastro/src/main/java/br/com/jitec/aps/cadastro/rest/payload/request/ClienteTelefoneRequest.java
@@ -6,19 +6,10 @@ import javax.validation.constraints.NotBlank;
 
 public class ClienteTelefoneRequest {
 
-	private UUID telefoneUid;
 	private UUID tipoTelefoneUid;
 
 	@NotBlank
 	private Integer numero;
-
-	public UUID getTelefoneUid() {
-		return telefoneUid;
-	}
-
-	public void setTelefoneUid(UUID telefoneUid) {
-		this.telefoneUid = telefoneUid;
-	}
 
 	public UUID getTipoTelefoneUid() {
 		return tipoTelefoneUid;
