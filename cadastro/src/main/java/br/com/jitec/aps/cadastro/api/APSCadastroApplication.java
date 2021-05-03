b475fd66d57537d8f03be0194357eae3c3df02ec
Isabel Tiburski <itiburski@gmail.com>
2021-05-03 07:55:21 -0300

    Integrate Firebase JWT Authentication

--- cadastro/src/main/java/br/com/jitec/aps/cadastro/api/APSCadastroApplication.java
+++ cadastro/src/main/java/br/com/jitec/aps/cadastro/api/APSCadastroApplication.java
@@ -2,20 +2,35 @@ package br.com.jitec.aps.cadastro.api;
 
 import javax.ws.rs.core.Application;
 
+import org.eclipse.microprofile.openapi.annotations.Components;
 import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeType;
 import org.eclipse.microprofile.openapi.annotations.info.Info;
 import org.eclipse.microprofile.openapi.annotations.info.License;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityRequirement;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityScheme;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 @OpenAPIDefinition(
-	tags = { @Tag(name = CadastroApiConstants.TAG_CATEGORIAS_CLIENTE, description = CadastroApiConstants.TAG_CATEGORIAS_CLIENTE_DESCRIPTION),
+	tags = { 
+			@Tag(name = CadastroApiConstants.TAG_CATEGORIAS_CLIENTE, description = CadastroApiConstants.TAG_CATEGORIAS_CLIENTE_DESCRIPTION),
 			@Tag(name = CadastroApiConstants.TAG_CIDADES, description = CadastroApiConstants.TAG_CIDADES_DESCRIPTION),
 			@Tag(name = CadastroApiConstants.TAG_CLIENTES, description =  CadastroApiConstants.TAG_CLIENTES_DESCRIPTION),
-			@Tag(name = CadastroApiConstants.TAG_TIPOS_TELEFONE, description =  CadastroApiConstants.TAG_TIPOS_TELEFONE_DESCRIPTION) },
-    info = @Info(
+			@Tag(name = CadastroApiConstants.TAG_TIPOS_TELEFONE, description =  CadastroApiConstants.TAG_TIPOS_TELEFONE_DESCRIPTION) 
+	},
+	info = @Info(
 			title = CadastroApiConstants.API_TITLE, description = CadastroApiConstants.API_DESCRIPTION,
 			version = CadastroApiConstants.API_VERSION,
-    license = @License(name = CadastroApiConstants.LICENSE_NAME, url = CadastroApiConstants.LICENSE_URL))
+			license = @License(name = CadastroApiConstants.LICENSE_NAME, url = CadastroApiConstants.LICENSE_URL)
+	),
+	security = {
+			@SecurityRequirement(name = "cadastroSecurityScheme")
+	},
+	components = @Components(
+			securitySchemes = {
+					@SecurityScheme(securitySchemeName = "cadastroSecurityScheme", type = SecuritySchemeType.HTTP, scheme = "Bearer", bearerFormat = "JWT") 
+			}
+	)
 )
 public class APSCadastroApplication extends Application {
 
--- servico/src/main/java/br/com/jitec/aps/servico/api/APSServicoApplication.java
+++ servico/src/main/java/br/com/jitec/aps/servico/api/APSServicoApplication.java
@@ -2,21 +2,36 @@ package br.com.jitec.aps.servico.api;
 
 import javax.ws.rs.core.Application;
 
+import org.eclipse.microprofile.openapi.annotations.Components;
 import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeType;
 import org.eclipse.microprofile.openapi.annotations.info.Info;
 import org.eclipse.microprofile.openapi.annotations.info.License;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityRequirement;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityScheme;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 @OpenAPIDefinition(
-	tags = { @Tag(name = ServicoApiConstants.TAG_ORDENS_SERVICO, description =  ServicoApiConstants.TAG_ORDENS_SERVICO_DESCRIPTION),
+	tags = { 
+			@Tag(name = ServicoApiConstants.TAG_ORDENS_SERVICO, description =  ServicoApiConstants.TAG_ORDENS_SERVICO_DESCRIPTION),
 			@Tag(name = ServicoApiConstants.TAG_FATURAS, description =  ServicoApiConstants.TAG_FATURAS_DESCRIPTION),
 			@Tag(name = ServicoApiConstants.TAG_BAIXAS, description =  ServicoApiConstants.TAG_BAIXAS_DESCRIPTION),
 			@Tag(name = ServicoApiConstants.TAG_TIPOS_BAIXA, description =  ServicoApiConstants.TAG_TIPOS_BAIXA_DESCRIPTION),
-			@Tag(name = ServicoApiConstants.TAG_TIPOS_SERVICO, description = ServicoApiConstants.TAG_TIPOS_SERVICO_DESCRIPTION) },
+			@Tag(name = ServicoApiConstants.TAG_TIPOS_SERVICO, description = ServicoApiConstants.TAG_TIPOS_SERVICO_DESCRIPTION) 
+	},
     info = @Info(
 			title = ServicoApiConstants.API_TITLE, description = ServicoApiConstants.API_DESCRIPTION,
 			version = ServicoApiConstants.API_VERSION,
-    license = @License(name = ServicoApiConstants.LICENSE_NAME, url = ServicoApiConstants.LICENSE_URL))
+			license = @License(name = ServicoApiConstants.LICENSE_NAME, url = ServicoApiConstants.LICENSE_URL)
+	),
+    security = {
+            @SecurityRequirement(name = "servicoSecurityScheme")
+    },
+    components = @Components(
+            securitySchemes = {
+            		@SecurityScheme(securitySchemeName = "servicoSecurityScheme", type = SecuritySchemeType.HTTP, scheme = "Bearer", bearerFormat = "JWT")
+            }
+    )
 )
 public class APSServicoApplication extends Application {
 
